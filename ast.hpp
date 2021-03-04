4998980f07a8423fba9aace534aae06a943e6188
zackkhaz1 <zack.khaz@gmail.com>
2021-03-03 21:11:00 -0600

    Added class declarations for all nodes according to inheritance diagram

--- ast.hpp
+++ ast.hpp
@@ -31,7 +31,7 @@ public:
 	/**
 	* Return a string specifying the position this node begins.
 	* For some node a position doesn't really make sense (i.e.
-	* ProgramNode) but for the rest it's the position in the 
+	* ProgramNode) but for the rest it's the position in the
 	* input file that represents that node
 	**/
 	std::string pos(){
@@ -44,7 +44,7 @@ private:
 	size_t c; /// The column at which the node starts in the input file
 };
 
-/** 
+/**
 * \class ProgramNode
 * Class that contains the entire abstract syntax tree for a program.
 * Note the list of declarations encompasses all global declarations
@@ -53,7 +53,7 @@ private:
 **/
 class ProgramNode : public ASTNode{
 public:
-	ProgramNode(std::list<DeclNode *> * globalsIn) 
+	ProgramNode(std::list<DeclNode *> * globalsIn)
 	: ASTNode(1, 1), myGlobals(globalsIn){
 	}
 	void unparse(std::ostream& out, int indent) override;
@@ -63,14 +63,15 @@ private:
 
 
 /** \class DeclNode
-* Superclass for declarations (i.e. nodes that can be used to 
-* declare a struct, function, variable, etc).  This base class will 
+* Superclass for declarations (i.e. nodes that can be used to
+* declare a struct, function, variable, etc).  This base class will
 **/
 class DeclNode : public ASTNode{
 public:
-	DeclNode(size_t line, size_t col) 
+	DeclNode(size_t line, size_t col)
 	: ASTNode(line, col) {
 	}
+
 	void unparse(std::ostream& out, int indent) override = 0;
 };
 
@@ -81,33 +82,33 @@ public:
 **/
 class ExpNode : public ASTNode{
 protected:
-	ExpNode(size_t line, size_t col) 
+	ExpNode(size_t line, size_t col)
 	: ASTNode(line, col){
 	}
 };
 
 /**  \class TypeNode
-* Superclass of nodes that indicate a data type. For example, in 
+* Superclass of nodes that indicate a data type. For example, in
 * the declaration "int a", the int part is the type node (a is an IDNode
 * and the whole thing is a DeclNode).
 **/
 class TypeNode : public ASTNode{
 protected:
-	TypeNode(size_t lineIn, size_t colIn) 
+	TypeNode(size_t lineIn, size_t colIn)
 	: ASTNode(lineIn, colIn){
 	}
 public:
 	virtual void unparse(std::ostream& out, int indent) = 0;
-	//TODO: consider adding an isRef to use in unparse to 
+	//TODO: consider adding an isRef to use in unparse to
 	// indicate if this is a reference type
 };
 
 /** An identifier. Note that IDNodes subclass
- * ExpNode because they can be used as part of an expression. 
+ * ExpNode because they can be used as part of an expression.
 **/
-class IDNode : public ExpNode{
+class IDNode : public LValNode{
 public:
-	IDNode(IDToken * token) 
+	IDNode(IDToken * token)
 	: ExpNode(token->line(), token->col()), myStrVal(token->value()){
 		myStrVal = token->value();
 	}
@@ -117,22 +118,9 @@ private:
 	std::string myStrVal;
 };
 
- 
-/** A variable declaration. Note that this class is intended to 
- * represent a global or local variable of any type (including a struct
- * type. Note that this is not intended to represent a declaration of
- * a struct. In other words:
- * struct MyStruct { 
- *   int fieldA;
- * };
- * is NOT a VarDeclNode because it introduces a new datatype, not a new
- * variable (in this case, the example is a StructDeclNode).  * However,
- * struct MyStruct instance; *is* a VarDeclNode, since it introduces a 
- * new variable to the program. 
-**/
 class VarDeclNode : public DeclNode{
 public:
-	VarDeclNode(size_t l, size_t c, TypeNode * type, IDNode * id) 
+	VarDeclNode(size_t l, size_t c, TypeNode * type, IDNode * id)
 	: DeclNode(type->line(), type->col()), myType(type), myId(id){
 	}
 	void unparse(std::ostream& out, int indent);
@@ -141,12 +129,190 @@ private:
 	IDNode * myId;
 };
 
+class FormalDeclNode : public VarDeclNode{
+public:
+	FormalDeclNode(size_t l, size_t c, TypeNode* type, IDNode* id)
+		: VarDeclNode(type->line(), type->col()), myType(type), myId(id){
+}
+private:
+	TypeNode* myType;
+	IDNode* myId;
+};
+
+class FnDeclNode : public DeclNode{
+public:
+	FnDeclNode(size_t l, size_t, c, TypeNode* type, IDNode* id, std::list<FormalDeclNode*>* params, std::list<StmtNode*>* body)
+			:  DeclNode(type->line(), type->col()), myType(type), myId(id){
+			}
+private:
+	TypeNode* myType;
+	IDNode* myId;
+};
+
+
+///////STMTNODE CLASSES////////////
+///////////////////////////////////
+
+class IfElseStmtNode : public StmtNode{
+
+};
+
+class IfStmtNode : public StmtNode{
+
+};
+
+class PostDecStmtNode : public StmtNode{
+
+};
+
+class PostIncStmtNode : public StmtNode{
+
+};
+
+class ReadStmtNode : public StmtNode{
+
+};
+
+class ReturnStmtNode : public StmtNode{
+
+};
+
+class whileStmtNode : public StmtNode{
+
+};
+
+class WriteStmtNode : public StmtNode{
+
+};
+
+class AssignStmtNode : public StmtNode{
+
+};
+
+class CallStmtNode : public StmtNode{
+
+};
+
+
+///////TYPENODE CLASSES////////////
+///////////////////////////////////
+
+class ArrayTypeNode : public TypeNode{
+
+};
+
+class BoolTypeNode : public TypeNode{
+
+};
+
+class ByteTypeNode : public TypeNode{
+
+};
+
+class IntTypeNode : public TypeNode{
+
+};
+
+class VoidTypeNode : public TypeNode{
+
+};
+
 class IntTypeNode : public TypeNode{
 public:
 	IntTypeNode(size_t lineIn, size_t colIn)
-	: TypeNode(lineIn, colIn){ 
+	: TypeNode(lineIn, colIn){
 	}
 	void unparse(std::ostream& out, int indent);
+};
+
+///////EXPNODE CLASSES//////////////
+///////////////////////////////////
+
+class AssignExpNode : public ExpNode{
+
+};
+
+class BinaryExpNode : public ExpNode{
+
+};
+
+class CallExpNode : public ExpNode{
+
+};
+
+class FalseNode : public ExpNode{
+
+};
+
+class HavocNode : public ExpNode{
+
+};
+
+class IntLitNode : public ExpNode{
+
+};
+
+class LValNode : public ExpNode{
+
+};
+
+class StrLitNode : public ExpNode{
+
+};
+
+class TrueNode : public ExpNode{
+
+};
+
+class UnaryExpNode : public ExpNode{
+
+};
+
+///////BINARYEXPNODE SUBCLASSES//////////////
+////////////////////////////////////////////
+
+class AndNode : public BinaryExpNode{
+
+};
+
+class DivideNode : public BinaryExpNode{
+
+};
+
+class EqualsNode : public BinaryExpNode{
+
+};
+
+class GreateEqNode : public BinaryExpNode{
+
+};
+
+class GreaterNode : public BinaryExpNode{
+
+};
+
+class LessEqNode : public BinaryExpNode{
+
+};
+
+class MinusNode : public BinaryExpNode{
+
+};
+
+class NotEqualsNode : public BinaryExpNode{
+
+};
+
+class OrNode : public BinaryExpNode{
+
+};
+
+class PlusNode : public BinaryExpNode{
+
+};
+
+class TimesNode : public BinaryExpNode{
+
 };
 
 } //End namespace crona
